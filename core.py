d1efd120cbff9ea551c39d34d253e8d367795859
thoth <xolotl15@gmail.com>
2022-04-10 21:59:35 -0500

    first working version

--- core.py
+++ core.py
@@ -1,84 +1,123 @@
-from random import randint
-from secrets import choice
+import asyncio
 
-# lista con letras del abecedario
+def game():
+    from random import randint
+    from secrets import choice
 
-abc : list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    # lista con letras del abecedario
+
+    abc : list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 
-# crear matriz con espacios 'vacíos'
+    # crear matriz con espacios 'vacíos'
 
-row : int = 20
-column : int = 20
-matrix : list = [[0 for j in range(0,column)] for i in range(0,row)]
+    row : int = 20
+    column : int = 20
+    matrix : list = [['-' for j in range(0,column)] for i in range(0,row)]
 
-# extraer lista de palabras de .txt y crear una lista con ellas
+    # extraer lista de palabras de .txt y crear una lista con ellas
 
-f = open('word_list.txt','r',encoding='utf-8')
-word_list = [word.replace('\n','') for word in f.readlines()]
+    f = open('word_list.txt','r',encoding='utf-8')
+    word_list = [word.replace('\n','') for word in f.readlines()]
+    f.close()
 
-# tomar palabra de lista
+    # # tomar palabra de lista
 
-word = choice(word_list)
+    # word = choice(word_list)
 
-# decidir coordenadas iniciales
-i = randint(0,row - 1)
-j = randint(0,column - 1)
+    # # decidir coordenadas iniciales
+    # i = randint(0,row - 1)
+    # j = randint(0,column - 1)
 
-print (i,j)
+    # print (i,j)
 
-# función para introducir nueva palabra
-def direction( theta:str , i:int , j:int , word:str = 'test'):
-    for letter in word.upper():
+    # función para introducir nueva palabra
+    def direction( theta:int , i:int , j:int , word:str):
+        for letter in word.upper():
+            
+            if ( (row > i and i >= 0) and (column > j and j >= 0) ) and (matrix [i][j] == '-' or matrix[i][j] == letter):
+                matrix[i][j] = letter
+            else:
+                raise(RuntimeError)
+            
+            if matrix [i][j] == '-' or matrix[i][j] == letter:
+                matrix[i][j] = letter
+
+            if theta == 1:
+                i -= 1
+                j += 1
+
+            elif theta == 2:
+                j += 1
+
+            elif theta == 3:
+                i += 1
+                j += 1
+
+            elif theta == 4:
+                i += 1
+
+            elif theta == 5:
+                i += 1
+                j -= 1
+
+            elif theta == 6:
+                j -= 1
         
-        if ( (row > i and i >= 0) and (column > j and j >= 0) ) and (matrix [i][j] == 0 or matrix[i][j] == letter):
-            matrix[i][j] = letter
-        else:
-            raise(RuntimeError)
-        
-        if matrix [i][j] == 0 or matrix[i][j] == letter:
-            matrix[i][j] = letter
+            elif theta == 7:
+                i -= 1
+                j -= 1
 
-        if theta == 'NE':
-            i -= 1
-            j += 1
+            elif theta == 8:
+                i -= 1
 
-        elif theta == 'E':
-            j += 1
+    # matar loop
 
-        elif theta == 'SE':
-            i += 1
-            j += 1
+    time = 1
 
-        elif theta == 'S':
-            i += 1
+    # Temporal
+    for word in word_list:
 
-        elif theta == 'SO':
-            i += 1
-            j -= 1
+        # Deciding initial position
 
-        elif theta == 'O':
-            j -= 1
-    
-        elif theta == 'NO':
-            i -= 1
-            j -= 1
+        i = randint(0,row - 1)
+        j = randint(0,column - 1)
 
-        elif theta == 'N':
-            i -= 1
+        # Writing word
+        theta = 1
 
-# verificar espación disponibles
+        while theta < 9:
+            time +=1
+            if time > 300:
+                raise ValueError
 
+            try:
+                direction( theta , i , j ,word)
+                break
+            except RuntimeError:
+                theta += 1
+            if theta == 9:
+                theta = 1
+                i = randint(0,row - 1)
+                j = randint(0,column - 1)
 
-# Temporal
-direction('N',i,j,word)
+    # rellenar espacios vacíos con letras aleatorias
+    # for i in matrix:
+    #     print(i)
 
-# rellenar espacios vacíos con letras aleatorias
+    # print('\n')
 
-for i in range(20):
-    for j in range(20):
-        if matrix[i][j] == 0:
-            matrix[i][j] = choice(abc)
+    for i in range(row):
+        for j in range(column):
+            if matrix[i][j] == '-':
+                matrix[i][j] = choice(abc)
 
-for i in matrix:
-    print(i)
+    for i in matrix:
+        print(i)
+
+while True:
+    try:
+        game()
+        break
+    except ValueError:
+        pass
--- test.py
+++ test.py
@@ -1,27 +1,45 @@
-l =[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'O', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'U', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'D', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'I', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'V', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'I', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'D', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'N', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'I', 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+# Importing neccesary libraries and modules
 
-for i in range(20):
-    for j in range(20):
-        if l[i][j] == 0:
-            l[i][j] = 1
+from random import randint
+from secrets import choice
+from core import direction
 
-print (l)
+# Creating list of alphabet letters, to randomly fill in the empty spaces
+
+abc : list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+# Creating matrix filled in with zeros, the number of columns and rows have to be entered
+
+row : int = int( input( 'Enter the number of rows:\n>>> ' ) )
+column : int = int( input( 'Enter the number of columns\n>>> ' ) )
+
+matrix : list = [[0 for j in range(0,column)] for i in range(0,row)]
+
+# Getting list of words from a .txt file
+
+f = open('word_list.txt','r',encoding='utf-8')
+word_list = [word.replace('\n','') for word in f.readlines()]
+f.close()
+
+###
+
+for word in word_list:
+
+    # Deciding initial position
+
+    i = randint(0,row - 1)
+    j = randint(0,column - 1)
+
+    # Writing word
+    theta = 1
+
+    while theta < 9:
+        
+        try:
+            direction( 1 , i , j ,word , row , column , matrix)
+            break
+        except RuntimeError:
+            theta += 1
+
+for i in matrix:
+    print(i)
